6f1c77fe3caff95d76f1bd1ed1efd3c5654b0c8d
Vinícius Vasques <vinivasq@hotmail.com>
2023-02-01 16:46:29 -0300

    layout desktop para adição de imagem de produto

---
+++ assets/scripts/drag-and-drop.js
@@ -0,0 +1,12 @@
+const dropZone = document.querySelector("[data-dropzone]")
+const inputFile = document.querySelector('#input__file')
+
+function dropHandler(event){
+    event.preventDefault()
+    
+}
+
+function dragOverHandler(event) {
+    event.preventDefault()
+
+}
