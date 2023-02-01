cb99edfe2a485df235692d9104554dfe8e89b59d
Vinícius Vasques <vinivasq@hotmail.com>
2023-02-01 20:57:08 -0300

    funcionalidade drag n drop e seleção de arquivos no desktop

--- assets/scripts/drag-and-drop.js
+++ assets/scripts/drag-and-drop.js
@@ -1,12 +1,51 @@
-const dropZone = document.querySelector("[data-dropzone]")
+const dropzoneContainer = document.querySelector(".dropzone__container")
+const dropZone = document.querySelector('[data-dropzone]')
 const inputFile = document.querySelector('#input__file')
+const errorMessage = document.querySelector('[data-imageSelector-error]')
+const filesToUpload = []
 
-function dropHandler(event){
-    event.preventDefault()
+const fileHandler = (file, name, type) => {
+    if(type.split('/')[0] !== 'image'){
+        errorMessage.innerHTML = `<i class="fa-solid fa-circle-info"></i> Tipo de arquivo inválido. Por favor, selecione uma imagem.`
+        
+        return false
+    }
     
-}
+    errorMessage.innerHTML = ''
 
-function dragOverHandler(event) {
-    event.preventDefault()
+    filesToUpload.push(file)
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = () => {
+        dropZone.innerHTML = ''
+        const img = document.createElement("img");
+        const imgDescription = document.createElement("p")
+        imgDescription.classList.add('dropzone__description')
+        imgDescription.innerHTML = name
+        img.src = reader.result
+        dropZone.appendChild(img)
+        dropzoneContainer.appendChild(imgDescription )
+    }
+} 
 
-}
+inputFile.addEventListener('change', (e) => {
+    e.preventDefault()
+    Array.from(inputFile.files).forEach(file => {
+        fileHandler(file, file.name, file.type)
+    })
+    console.log(filesToUpload);
+})
+
+dropZone.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const files = e.dataTransfer.files
+    console.log(files);
+    Array.from(files).forEach(file => {
+        fileHandler(file, file.name, file.type)
+    })
+    console.log(filesToUpload);
+})
+
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault()
+})
