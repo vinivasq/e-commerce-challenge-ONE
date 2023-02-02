220884ceba8d9b49f0ba43394474923e94eafef0
Vinícius Vasques <vinivasq@hotmail.com>
2023-02-02 16:55:20 -0300

    estilização e responsividade no seletor de imagem

--- assets/scripts/drag-and-drop.js
+++ assets/scripts/drag-and-drop.js
@@ -1,51 +1,56 @@
-const dropzoneContainer = document.querySelector(".dropzone__container")
-const dropZone = document.querySelector('[data-dropzone]')
-const inputFile = document.querySelector('#input__file')
-const errorMessage = document.querySelector('[data-imageSelector-error]')
-const filesToUpload = []
-
 const fileHandler = (file, name, type) => {
     if(type.split('/')[0] !== 'image'){
         errorMessage.innerHTML = `<i class="fa-solid fa-circle-info"></i> Tipo de arquivo inválido. Por favor, selecione uma imagem.`
         
         return false
     }
-    
     errorMessage.innerHTML = ''
-
-    filesToUpload.push(file)
+    
+    const imgName = document.querySelector('.dropzone__description')
+    if(imgName != null) imgName.remove()
+    
     const reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onloadend = () => {
         dropZone.innerHTML = ''
+        dropZone.classList.add('filled')
         const img = document.createElement("img");
-        const imgDescription = document.createElement("p")
+        const imgDescription = document.createElement("span")
         imgDescription.classList.add('dropzone__description')
         imgDescription.innerHTML = name
         img.src = reader.result
         dropZone.appendChild(img)
-        dropzoneContainer.appendChild(imgDescription )
+        dropZoneContainer.appendChild(imgDescription )
     }
 } 
 
+const dropZoneContainer = document.querySelector(".dropzone__container")
+const dropZone = document.querySelector('[data-dropzone]')
+const inputFile = document.querySelector('#input__file')
+const errorMessage = document.querySelector('[data-imageSelector-error]')
+
 inputFile.addEventListener('change', (e) => {
     e.preventDefault()
-    Array.from(inputFile.files).forEach(file => {
-        fileHandler(file, file.name, file.type)
-    })
-    console.log(filesToUpload);
+    const file = Array.from(inputFile.files)[0] //gets only the first file, so we allow just one image
+    fileHandler(file, file.name, file.type)
 })
-
+    
 dropZone.addEventListener('drop', (e) => {
     e.preventDefault()
-    const files = e.dataTransfer.files
-    console.log(files);
-    Array.from(files).forEach(file => {
-        fileHandler(file, file.name, file.type)
-    })
-    console.log(filesToUpload);
+    const file = Array.from(e.dataTransfer.files)[0]
+    fileHandler(file, file.name, file.type)
 })
 
 dropZone.addEventListener('dragover', (e) => {
     e.preventDefault()
+})
+
+dropZone.addEventListener('dragenter', (e) => {
+    e.preventDefault()
+    dropZone.classList.add('active')
+})
+
+dropZone.addEventListener('dragleave', (e) => {
+    e.preventDefault()
+    dropZone.classList.remove('active')
 })
