c5d48e18999e043c92695580998be3b730170da0
Vinícius Vasques <vinivasq@hotmail.com>
2023-02-05 11:58:03 -0300

    tratamento da listagem com try catch

--- assets/scripts/controller/listProduct.js
+++ assets/scripts/controller/listProduct.js
@@ -27,32 +27,42 @@ const categories = document.querySelectorAll('.categorie')
 
 
 const render = async() => {
-    const products = await productService.listProducts()
-    
-    products.forEach(product => {
-        const categoriesNames = []
+    try {
+        const products = await productService.listProducts()
         
-        categories.forEach(category => {
-            const categoryName = category.firstElementChild.innerHTML
-            categoriesNames.push(categoryName)
-
-            if(categoryName.toLowerCase() == product.category.toLowerCase()){
-                const list = category.querySelector(".products__list")
-                const newProduct = document.createElement("li")
-                newProduct.classList.add("list__item")
-                newProduct.innerHTML = createProduct(product.image, product.name, product.price)
-                list.appendChild(newProduct)
+        products.forEach(product => {
+            const categoriesNames = []
+            
+            categories.forEach(category => {
+                const categoryName = category.firstElementChild.innerHTML
+                categoriesNames.push(categoryName)
+    
+                if(categoryName.toLowerCase() == product.category.toLowerCase()){
+                    const list = category.querySelector(".products__list")
+                    const newProduct = document.createElement("li")
+                    newProduct.classList.add("list__item")
+                    newProduct.innerHTML = createProduct(product.image, product.name, product.price)
+                    list.appendChild(newProduct)
+                }
+            })
+    
+            if (!categoriesNames.some(category => category.toLowerCase() == product.category.toLowerCase())) {
+                const newCategory = document.createElement("section")
+                newCategory.classList.add("categorie")
+                newCategory.innerHTML = createCategory(product.category, 
+                    createProduct(product.image, product.name, product.price))
+                categories[categories.length - 1].parentElement.appendChild(newCategory)
             }
         })
+    } catch (error) {
+        console.log(error);
+        
+        alert(`
+            Não foi possível listar os produtos presentes no banco de dados.
+            Tente iniciar o json-server.
+        `)
+    }
 
-        if (!categoriesNames.some(category => category.toLowerCase() == product.category.toLowerCase())) {
-            const newCategory = document.createElement("section")
-            newCategory.classList.add("categorie")
-            newCategory.innerHTML = createCategory(product.category, 
-                createProduct(product.image, product.name, product.price))
-            categories[categories.length - 1].parentElement.appendChild(newCategory)
-        }
-    })
 }
 
 render()
